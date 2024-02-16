5b36903189cc9400cc7f10dc02760ac2f5053696
Smit Waman <159112812+smitwaman@users.noreply.github.com>
2024-02-17 00:29:51 +0530

    Create ec2.tf

---
+++ ec2.tf
@@ -0,0 +1,16 @@
+resource "aws_instance" "public_instance" {
+  ami                    = "ami-05fb0b8c1424f266b"
+  instance_type          = "t2.micro"
+  key_name               = aws_key_pair.key_pair.key_name
+  vpc_security_group_ids = [aws_security_group.sg_ec2.id]
+  subnet_id              = aws_subnet.public.id
+  associate_public_ip_address = true
+
+  tags = {
+    Name = "public_instance"
+}
+}
+
+output "public_ip" {
+  value = aws_instance.public_instance.public_ip
+}
