f9fb355428dbad2200e800fb32a6c36857ba956a
Smit Waman <159112812+smitwaman@users.noreply.github.com>
2024-02-16 15:41:31 +0530

    Update main.tf

--- modules/ec2/main.tf
+++ modules/ec2/main.tf
@@ -1,5 +1,8 @@
 # modules/ec2_instance/main.tf
-
+variable "ami_id" {}
+variable "instance_type" {}
+variable "key_name" {}
+variable "security_group_ids" {}
 
 
 resource "aws_instance" "example" {
