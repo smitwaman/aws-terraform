80ccfc7377921000575c23a8caa20a79e95ddd71
Smit Waman <159112812+smitwaman@users.noreply.github.com>
2024-02-16 22:16:22 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,64 +1,94 @@
-module "vpc" {
-  source   = "./modules/vpc"
-  vpc_cidr = "10.0.0.0/16"
+
+# Create VPC
+resource "aws_vpc" "aws-terraform" {
+  cidr_block = "10.0.0.0/16"
+}
+
+# Create internet gateway
+resource "aws_internet_gateway" "IGW" {
+  vpc_id = aws_vpc.aws-terraform.id
+}
+
+# Create public subnet
+resource "aws_subnet" "public" {
+  vpc_id                  = aws_vpc.aws-terraform.id
+  cidr_block              = "10.0.1.0/24"
+  map_public_ip_on_launch = true
+}
+
+# Create route table for public subnet
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.aws-terraform.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.IGW.id
+  }
+}
+
+# Associate public subnet with public route table
+resource "aws_route_table_association" "public_subnet_association" {
+  subnet_id      = aws_subnet.public.id
+  route_table_id = aws_route_table.public.id
 }
 
 
 
-
-
-module "subnets" {
-  source                = "./modules/subnet"
-  vpc_id                = module.vpc.vpc_id
-  public_subnet_cidr_a  = "10.0.1.0/24"
-  public_subnet_cidr_b  = "10.0.2.0/24"
-  private_subnet_cidr_a = "10.0.3.0/24"
-  private_subnet_cidr_b = "10.0.4.0/24"
-  availability_zone_a   = "us-east-2a"
-  availability_zone_b   = "us-east-2b"
+// To Generate Private Key
+resource "tls_private_key" "rsa_4096" {
+  algorithm = "RSA"
+  rsa_bits  = 4096
 }
 
-module "igw" {
-  source = "./modules/igw"
-  vpc_id = module.vpc.vpc_id
+variable "key_name" {
+  description = "Enter key name"
 }
 
-module "nat_gateway" {
-  source           = "./modules/nat_gateway"
-  public_subnet_id = module.subnets.public_subnet_id_a
+// Create Key Pair for Connecting EC2 via SSH
+resource "aws_key_pair" "key_pair" {
+  key_name   = var.key_name
+  public_key = tls_private_key.rsa_4096.public_key_openssh
 }
 
-module "route_table" {
-  source              = "./modules/route_table"
-  vpc_id              = module.vpc.vpc_id
-  igw_id              = module.igw.igw_id
-  nat_gateway_id      = module.nat_gateway.nat_gateway_id
-  public_subnet_id_a  = module.subnets.public_subnet_id_a
-  public_subnet_id_b  = module.subnets.public_subnet_id_b
-  private_subnet_id_a = module.subnets.private_subnet_id_a
-  private_subnet_id_b = module.subnets.private_subnet_id_b
+// Save PEM file locally
+resource "local_file" "private_key" {
+  content  = tls_private_key.rsa_4096.private_key_pem
+  filename = var.key_name
 }
 
-module "security_group" {
-  source = "./modules/security_group"
-  vpc_id = module.vpc.vpc_id
+# Create a security group
+resource "aws_security_group" "sg_ec2" {
+  name        = "sg_ec2"
+  description = "Security group for EC2"
+  vpc_id      = aws_vpc.aws-terraform.id
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 }
 
-resource "aws_instance" "EC2" {
-  ami             = "ami-06b74af9fe7907906"  # Specify your desired AMI
-  instance_type   = "t2.micro"     # Specify your desired instance type
-  subnet_id       =  aws_subnet.public_subnet_b.id
-  security_groups = [aws_security_group.allow_web.name]
-
-  # Example: Provisioning script to install software
-  user_data = <<-EOF
-              #!/bin/bash
-              yum update -y
-              yum install -y docker
-              systemctl start docker
-              EOF
+resource "aws_instance" "public_instance" {
+  ami                    = "ami-05fb0b8c1424f266b"
+  instance_type          = "t2.micro"
+  key_name               = aws_key_pair.key_pair.key_name
+  vpc_security_group_ids = [aws_security_group.sg_ec2.id]
+  subnet_id              = aws_subnet.public.id
+  associate_public_ip_address = true
 
   tags = {
-    Name = "Instance"
-  }
+    Name = "public_instance"
+}
+}
+
+output "public_ip" {
+  value = aws_instance.public_instance.public_ip
 }
