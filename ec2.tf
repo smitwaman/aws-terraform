7543f128cb5cc6112c819a79995af2ad954c707b
Smit Waman <159112812+smitwaman@users.noreply.github.com>
2024-02-17 08:27:56 +0530

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -5,7 +5,12 @@ resource "aws_instance" "public_instance" {
   vpc_security_group_ids = [aws_security_group.sg_ec2.id]
   subnet_id              = aws_subnet.public.id
   associate_public_ip_address = true
+  ssh_keys = [var.ssh_key_fingerprint]  # SSH keys to be injected into the droplet
 
+  # Run the shell script after the droplet is created
+  provisioner "local-exec" {
+    command = "./install.sh"
+  }
   tags = {
     Name = "public_instance"
 }
